75d94f60ee091851b8562ae4107cd8abebb820a3
Jairo <jn.gomezm1@unaindes.edu.co>
2023-09-21 21:32:38 -0500

    Titulo Guarap

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:guarap/main.dart';
+import 'package:guarap/ui/publish_photo.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PublishPhoto());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
