b120d5003266938e10acc7fc41be8dc6df4edd7a
Daniel Osorio <d.osorioc2@uniandes.edu.co>
2023-10-03 13:21:42 -0500

    Configure firebase version and add auth folder for login and account creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:guarap/components/home/ui/home.dart';
 import 'package:guarap/components/publish_photos/ui/publish_photo.dart';
@@ -6,7 +8,10 @@ var kColorScheme = ColorScheme.fromSeed(
     seedColor: const Color.fromARGB(
         0, 0, 0, 0)); // Define the color palette for the app
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
