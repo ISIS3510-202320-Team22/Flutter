3d940124205c67e1d0cd49490826ec28e106ffb0
Xavi85 <j.serranor2@uniandes.edu.co>
2023-10-03 22:23:22 -0300

    Categories

--- lib/components/home/bloc/home_event.dart
+++ lib/components/home/bloc/home_event.dart
@@ -15,6 +15,11 @@ class HomePublishButtonClickedEvent extends HomeEvent{
 
 }
 
+// Event when the categories button is clicked
+class HomeCategoriesButtonClickedEvent extends HomeEvent{
+
+}
+
 // Event when the profile button is clicked to navigate to this page
 class HomeProfileButtonNavigateEvent extends HomeEvent{
 
@@ -23,5 +28,10 @@ class HomeProfileButtonNavigateEvent extends HomeEvent{
 
 // Event when the publish button is clicked to navigate to this page
 class HomePublishButtonNavigateEvent extends HomeEvent{
+
+}
+
+// Event when the categories button is clicked to navigate to this page
+class HomeCategoriesButtonNavigateEvent extends HomeEvent{
 
 }
