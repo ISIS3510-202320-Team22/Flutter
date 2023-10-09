44b0c99a65adba3fbf8322acd6899834ef0c37ba
Xavi85 <j.serranor2@uniandes.edu.co>
2023-10-09 11:57:47 -0300

    Prueba

--- lib/components/categories/ui/categories.dart
+++ lib/components/categories/ui/categories.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+//Comenatrio prueba
+
 class Categories extends StatefulWidget {
   const Categories({super.key});
 
